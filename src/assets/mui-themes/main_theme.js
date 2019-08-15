11716aa47fbcc148ba5a1e6d55c3ad0737efd98c
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-08-15 16:00:29 -0400

    Working on filling out the Tech Stack component of the main page.

--- src/assets/mui-themes/main_theme.js
+++ src/assets/mui-themes/main_theme.js
@@ -1,14 +1,143 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
+import css3_logo from '../logos/css3_logo.svg';
+import express_logo from '../logos/express_logo.svg';
+import github_logo from '../logos/github_logo.svg';
+import html5_logo from '../logos/html5_logo.svg';
+import illustrator_logo from '../logos/illustrator_logo.svg';
+import javascript_logo from '../logos/javascript_logo.svg';
+import jest_logo from '../logos/jest_logo.svg';
+import jss_logo from '../logos/jss_logo.svg';
+import less_logo from '../logos/less_logo.svg';
+import materialui_logo from '../logos/materialui_logo.svg';
+import nodejs_logo from '../logos/nodejs_logo.svg';
+import postgresql_logo from '../logos/postgresql_logo.svg';
+import python_logo from '../logos/python_logo.svg';
+import react_logo from '../logos/react_logo.svg';
+import redux_logo from '../logos/redux_logo.svg';
+import sass_logo from '../logos/sass_logo.svg';
+import svg_logo from '../logos/svg_logo.svg';
+import xd_logo from '../logos/xd_logo.svg';
+
 export default createMuiTheme({
   variables: {
-    contactHeight: '500px'
+    contactHeight: '500px',
   },
   navLinks: {
     top: '#top',
     projects: '#projects',
     techStack: '#techStack',
-    contact: '#contact'
+    contact: '#contact',
+  },
+  logoProps: {
+    css3: {
+      text: 'CSS3',
+      link: 'https://www.w3.org/Style/CSS/current-work.en.html',
+      img: css3_logo,
+      alt: 'css3 logo',
+    },
+    express: {
+      text: 'Express',
+      link: 'https://expressjs.com/',
+      img: express_logo,
+      alt: 'express logo',
+    },
+    github: {
+      text: 'GitHub',
+      link: 'https://github.com/',
+      img: github_logo,
+      alt: 'github logo',
+    },
+    html5: {
+      text: 'HTML5',
+      link: 'https://www.w3.org/html/',
+      img: html5_logo,
+      alt: 'html5 logo',
+    },
+    illustrator: {
+      text: 'Adobe Illustrator',
+      link: 'https://helpx.adobe.com/illustrator/user-guide.html',
+      img: illustrator_logo,
+      alt: 'illustrator logo',
+    },
+    javascript: {
+      text: 'JavaScript',
+      link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference',
+      img: javascript_logo,
+      alt: 'javascript logo',
+    },
+    jest: {
+      text: 'Jest',
+      link: 'https://jestjs.io/',
+      img: jest_logo,
+      alt: 'jest logo',
+    },
+    jss: {
+      text: 'JSS',
+      link: 'https://cssinjs.org/',
+      img: jss_logo,
+      alt: 'jss logo',
+    },
+    less: {
+      text: 'less',
+      link: 'http://lesscss.org/',
+      img: less_logo,
+      alt: 'less logo',
+    },
+    materialui: {
+      text: 'Material-UI',
+      link: 'https://material-ui.com/',
+      img: materialui_logo,
+      alt: 'material-ui logo',
+    },
+    nodejs: {
+      text: 'Node.js',
+      link: 'https://nodejs.org/',
+      img: nodejs_logo,
+      alt: 'nodejs logo',
+    },
+    postgresql: {
+      text: 'PostgreSQL',
+      link: 'https://www.postgresql.org/',
+      img: postgresql_logo,
+      alt: 'postgresql logo',
+    },
+    python: {
+      text: 'Python',
+      link: 'https://www.python.org/',
+      img: python_logo,
+      alt: 'python logo',
+    },
+    react: {
+      text: 'React',
+      link: 'https://reactjs.org/',
+      img: react_logo,
+      alt: 'react logo',
+    },
+    redux: {
+      text: 'Redux',
+      link: 'https://redux.js.org/',
+      img: redux_logo,
+      alt: 'redux logo',
+    },
+    sass: {
+      text: 'Sass',
+      link: 'https://sass-lang.com/',
+      img: sass_logo,
+      alt: 'sass logo',
+    },
+    svg: {
+      text: 'SVG',
+      link: 'https://www.w3.org/Graphics/SVG/',
+      img: svg_logo,
+      alt: 'svg logo',
+    },
+    xd: {
+      text: 'Adobe XD',
+      link: 'https://helpx.adobe.com/xd/user-guide.html',
+      img: xd_logo,
+      alt: 'xd logo',
+    },
   },
   breakpoints: {
     values: {
@@ -17,8 +146,8 @@ export default createMuiTheme({
       sm: 600,
       md: 900,
       lg: 1200,
-      xl: 1800
-    }
+      xl: 1800,
+    },
   },
   palette: {
     // teal
@@ -26,38 +155,38 @@ export default createMuiTheme({
       light: '#8bffff',
       main: '#53d3d1',
       dark: '#00a1a0',
-      xdark: '#006666'
+      xdark: '#006666',
     },
     // pink
     secondary: {
       light: '#ffffff',
       main: '#FBECEB',
-      dark: '#c8bab9'
+      dark: '#c8bab9',
     },
     a: {
       // teal
       light: '#8bffff',
       main: '#53d3d1',
       dark: '#00a1a0',
-      xdark: '#006666'
+      xdark: '#006666',
     },
     b: {
       // pink
       light: '#ffffff',
       main: '#FBECEB',
-      dark: '#c8bab9'
+      dark: '#c8bab9',
     },
     c: {
       // purple
       light: '#344b73',
       main: '#002447',
-      dark: '#000020'
+      dark: '#000020',
     },
     d: {
       // orange
       light: '#ffe479',
       main: '#feb249',
-      dark: '#c78214'
-    }
-  }
+      dark: '#c78214',
+    },
+  },
 });
--- src/components/TechStack.js
+++ src/components/TechStack.js
@@ -3,30 +3,32 @@ import makeStyles from '@material-ui/styles/makeStyles';
 import useTheme from '@material-ui/styles/useTheme';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    width: '100%'
+    flexGrow: 1,
   },
   techStack: {
     [theme.breakpoints.down('xl')]: {
       paddingTop: '64px',
-      marginTop: '-64px'
+      marginTop: '-64px',
     },
     [theme.breakpoints.down('sm')]: {
       paddingTop: '56px',
-      marginTop: '-56px'
+      marginTop: '-56px',
     },
     [theme.breakpoints.down('xs')]: {
       paddingTop: '48px',
-      marginTop: '-48px'
-    }
+      marginTop: '-48px',
+    },
   },
   container: {
     background: [theme.palette.b.main],
     width: '100%',
-    height: '1000px',
-    padding: '15px'
+    height: '1750px',
+    padding: '15px',
   },
   headerDiv: {
     display: 'flex',
@@ -34,38 +36,57 @@ const useStyles = makeStyles(theme => ({
     alignItems: 'center',
     paddingBottom: '10px',
     [theme.breakpoints.only('xxs')]: {
-      flexDirection: 'column'
-    }
+      flexDirection: 'column',
+    },
   },
   title: {
     color: [theme.palette.primary.xdark],
     [theme.breakpoints.down('xl')]: {
-      fontSize: '3em'
+      fontSize: '3em',
     },
     [theme.breakpoints.down('xs')]: {
-      fontSize: '2.5em'
-    }
+      fontSize: '2.5em',
+    },
   },
   linkIconDiv: {
     [theme.breakpoints.down('xl')]: {
-      height: '64px'
+      height: '64px',
     },
     [theme.breakpoints.down('xs')]: {
-      height: '56px'
-    }
+      height: '56px',
+    },
   },
   linkIcon: {
     [theme.breakpoints.down('xl')]: {
-      height: '40px'
+      height: '40px',
     },
     [theme.breakpoints.down('xs')]: {
-      height: '32px'
-    }
+      height: '32px',
+    },
   },
   contentText: {
     color: [theme.palette.primary.xdark],
-    fontSize: '1.5em'
-  }
+    fontSize: '1.5em',
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  gridDiv: {
+    display: 'flex',
+  },
+  gridItem: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    textDecoration: 'none',
+  },
+  logo: {
+    width: '100%',
+    height: '100%',
+  },
 }));
 
 export default function Contact() {
@@ -85,6 +106,50 @@ export default function Contact() {
         <Typography style={{ color: [theme.palette.b.main] }}>
           {'<contact form goes here>'}
         </Typography>
+        <div className={classes.root}>
+          <div className={classes.gridDiv}>
+            <Grid container spacing={4}>
+              {[
+                theme.logoProps.css3,
+                theme.logoProps.express,
+                theme.logoProps.github,
+                theme.logoProps.html5,
+                theme.logoProps.illustrator,
+                theme.logoProps.javascript,
+                theme.logoProps.jest,
+                theme.logoProps.jss,
+                theme.logoProps.less,
+                theme.logoProps.materialui,
+                theme.logoProps.nodejs,
+                theme.logoProps.postgresql,
+                theme.logoProps.python,
+                theme.logoProps.react,
+                theme.logoProps.redux,
+                theme.logoProps.sass,
+                theme.logoProps.svg,
+                theme.logoProps.xd,
+              ].map(item => (
+                <Grid
+                  className={classes.gridItem}
+                  item
+                  xs={3}
+                  component='a'
+                  href={item.link}
+                  key={item.text}
+                >
+                  <Paper className={classes.paper}>
+                    <img
+                      className={classes.logo}
+                      src={item.img}
+                      alt={item.alt}
+                    />
+                    {item.text}
+                  </Paper>
+                </Grid>
+              ))}
+            </Grid>
+          </div>
+        </div>
       </Box>
     </div>
   );
